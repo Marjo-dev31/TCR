eee9a20d696bd2e5cd7bd66e1c04f9ce323d3d27
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-17 18:14:53 +0200

    css(home): animation done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
+const { transform } = require("typescript");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     fontFamily: {
-     lemon:["lemon", 'sans-serif']
+      lemon: ["lemon", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        blob: {
+          "0%": { transform: "translate(0px, 0px) scale(1)"},
+          "33%": { transform: "translate(30px, -50px) scale(1.2)"},
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)"},
+          "100%": { transform: "translate(0px, 0px) scale(1)"},
+        },
+      },
+      animation: {
+        blob: "blob 6s linear infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
