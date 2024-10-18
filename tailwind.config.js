8fbce91a6857fbce007c164865cb415b768474f1
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-18 17:46:52 +0200

    css(grid): border hover done

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,14 @@ module.exports = {
     extend: {
       keyframes: {
         blob: {
-          "0%": { transform: "translate(0px, 0px) scale(1)"},
-          "33%": { transform: "translate(30px, -50px) scale(1.2)"},
-          "66%": { transform: "translate(-20px, 20px) scale(0.9)"},
-          "100%": { transform: "translate(0px, 0px) scale(1)"},
+          "0%": { transform: "translate(0px, 0px) scale(1.1)"},
+          "33%": { transform: "translate(50px, -70px) scale(1)"},
+          "66%": { transform: "translate(-30px, 30px) scale(0.9)"},
+          "100%": { transform: "translate(0px, 0px) scale(1.1)"},
         },
       },
       animation: {
-        blob: "blob 6s linear infinite",
+        blob: "blob 7s linear infinite",
       },
     },
   },
