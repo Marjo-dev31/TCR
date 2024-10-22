29612fdba33fd0a772a250c0840433ed20946dad
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-22 17:57:23 +0200

    feat(documentation): documentation page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PressComponent } from './pages/press/press.component';
 import { PracticeComponent } from './pages/practice/practice.component';
+import { DocumentationComponent } from './pages/documentation/documentation.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'press', component: PressComponent},
     {path: 'practice', component: PracticeComponent},
+    {path: 'documentation', component: DocumentationComponent},
 ];
