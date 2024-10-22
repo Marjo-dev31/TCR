a8f4695a73e8a39225bbe3d930c3719a0b2d2791
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-22 18:20:05 +0200

    feat(galerie): component create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { PressComponent } from './pages/press/press.component';
 import { PracticeComponent } from './pages/practice/practice.component';
 import { DocumentationComponent } from './pages/documentation/documentation.component';
+import { PhotosComponent } from './pages/photos/photos.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'press', component: PressComponent},
     {path: 'practice', component: PracticeComponent},
     {path: 'documentation', component: DocumentationComponent},
+    {path: 'photos', component: PhotosComponent},
 ];
