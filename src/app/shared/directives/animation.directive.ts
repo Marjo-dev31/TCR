8e9628d0d689aededfe324ac7e58ca38fd222672
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-10-29 17:08:00 +0100

    merge dev

--- src/app/shared/directives/animation.directive.ts
+++ src/app/shared/directives/animation.directive.ts
@@ -1,12 +1,17 @@
-import { Directive, HostListener, signal } from '@angular/core';
+import { Directive, Host, HostListener, signal } from '@angular/core';
 
 @Directive({
   selector: '[appAnimation]',
-  standalone: true
+  standalone: true,
 })
 export class AnimationDirective {
+  constructor() {}
 
-  constructor() { }
+  @HostListener('click', ['$event'])
+  onClick($event: any) {
+    console.log('clicked' + $event);
+
+  }
 
   // @HostListener('click', ['$event'])
 
@@ -16,23 +21,23 @@ export class AnimationDirective {
   //   console.log('toto')
   //   this.openDefinition.update((s) => !s);
 
-    // const defElement = document.getElementById('def');
-    // const questionElement = document.getElementById('question');
-    // const definitionElement = document.getElementById('definition');
+  // const defElement = document.getElementById('def');
+  // const questionElement = document.getElementById('question');
+  // const definitionElement = document.getElementById('definition');
 
-    // if (this.openDefinition()) {
-    //   definitionElement?.classList.add('rotate-180');
-    //   definitionElement?.classList.add('-translate-x-full');
-    //   questionElement?.classList.add('opacity-0');
-    //   defElement?.classList.remove('opacity-0');
-    //   defElement?.classList.add('animate-fadeIn1');
-    // }
-    // if (!this.openDefinition()) {
-    //   definitionElement?.classList.remove('rotate-180');
-    //   definitionElement?.classList.remove('-translate-x-full');
-    //   questionElement?.classList.remove('opacity-0');
-    //   defElement?.classList.add('opacity-0');
-    //   defElement?.classList.remove('animate-fadeIn1');
-    // }
+  // if (this.openDefinition()) {
+  //   definitionElement?.classList.add('rotate-180');
+  //   definitionElement?.classList.add('-translate-x-full');
+  //   questionElement?.classList.add('opacity-0');
+  //   defElement?.classList.remove('opacity-0');
+  //   defElement?.classList.add('animate-fadeIn1');
+  // }
+  // if (!this.openDefinition()) {
+  //   definitionElement?.classList.remove('rotate-180');
+  //   definitionElement?.classList.remove('-translate-x-full');
+  //   questionElement?.classList.remove('opacity-0');
+  //   defElement?.classList.add('opacity-0');
+  //   defElement?.classList.remove('animate-fadeIn1');
+  // }
   // }
 }
